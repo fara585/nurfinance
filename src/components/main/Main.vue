8f9fab127a3313eff1ead40ed8c5490d19143155
fara585 <shomurodovhusan130@gmail.com>
2024-06-19 10:51:37 +0500

    changed

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -36,5 +36,74 @@
                 </div>
             </div>
         </div>
+
+        <section class="main__earn">
+            <div class="container">
+                <div class="main__earn-content">
+                    <h2>Рассчитай свои накопления и доход</h2>
+                    <div class="main__earn-content-btn">
+                        <div class="main__earn-content-btn-lft">
+                            <div class="main__earn-content-btn-range">
+                                <div class="main__earn-content-btn-range-top">
+                                    <p>Сумма вклада</p>
+                                    <output>1 000 000 ₽</output>
+                                </div>
+                                <input type="range">
+                            </div>
+                            <div class="main__earn-content-btn-range">
+                                <div class="main__earn-content-btn-range-top">
+                                    <p>Сумма вклада</p>
+                                    <output>1 000 000 ₽</output>
+                                </div>
+                                <input type="range">
+                            </div>
+                            <div class="main__earn-content-btn-check">
+                                <p>Порядок выплаты дохода</p>
+                                <div class="main__earn-content-btn-check-block">
+                                    <button>
+                                        <input type="checkbox">
+                                        <span>Ежемесячно</span>
+                                    </button>
+                                    <button>
+                                        <input type="checkbox">
+                                        <span>Ежемесячно</span>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="main__earn-content-btn-range">
+                                <div class="main__earn-content-btn-range-top">
+                                    <p>Сумма вклада</p>
+                                    <output>1 000 000 ₽</output>
+                                </div>
+                                <input type="range">
+                            </div>
+                        </div>
+                        <div class="main__earn-content-btn-center">
+                            <img src="@/assets/img/Frame.png" alt="">
+                        </div>
+                        <div class="main__earn-content-btn-right">
+                            <div class="main__earn-content-btn-right-top">
+                                <div class="main__earn-content-btn-right-per">
+                                    <output>12,5%</output>
+                                    <span>Доходность</span>
+                                </div>
+                                <div class="main__earn-content-btn-right-per">
+                                    <output>282 366 ₽</output>
+                                    <span>Сумма доходности</span>
+                                </div>
+                            </div>
+                            <div class="main__earn-content-btn-right-center">
+                                <output>1 282 366 ₽</output>
+                                <span>Итоговая сумма</span>
+                            </div>
+                            <button>
+                                Инвестировать
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
 </template>
